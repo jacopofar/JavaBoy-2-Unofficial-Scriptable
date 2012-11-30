c7b26c345359225e8c8eb20a4c105cc53194299a
jacopofar <jacopo1.farina@gmail.com>
2012-11-30 20:07:22 +0100

    Added EventRecorder, an automatism which record actions and screenshots from actual games

---
+++ src/automatisms/EventRecorder.java
@@ -0,0 +1,40 @@
+package automatisms;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import emulator.JavaBoy;
+
+/**
+ * Take a screenshot each time the user press a button, recording the time and the button pressed.
+ * */
+public class EventRecorder extends GameBoyListener{
+	GameHandle gh;
+	String folder;
+	public EventRecorder(GameHandle gh,String folderName){
+		this.gh=gh;
+		folder=folderName;
+		gh.addButtonListener(this);
+		//create the folder if doesn't exist
+		new File(folderName).mkdirs();
+	}
+
+	public boolean onButtonPressed(String key){
+		try {
+			ImageIO.write(gh.getScreenshot(), "PNG", new File(folder+File.separatorChar+new Date().getTime()+"_"+key+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public static void main(String[] args){
+		JavaBoy g=new JavaBoy("");
+		Thread p = new Thread(g);
+		p.start();
+		new EventRecorder(new GameHandle(g),"testfolder");
+	}
+}
